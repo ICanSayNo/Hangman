5365da544a658a6993dec53fcd74fe61266c1c64
League <league@WTS9.attlocal.net>
2018-06-11 20:00:24 -0700

    thank. hangmans

--- src/HangMan.java
+++ src/HangMan.java
@@ -4,41 +4,61 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class HangMan {
-	public static void main(String[] args) {
-		HangMan h = new HangMan();
-		int lengthOfList = h.SetNumber();
-		
-		ArrayList<String> items = new ArrayList<String>();
-		String output="";
-			JFileChooser fileExplorer = new JFileChooser();
-			fileExplorer.showOpenDialog(null);
-			
-		//	try {
-				
-				//for (int i = 0; i < lengthOfList; i++) {
-					
-					//items.add(word);
-			//	}
-				//JOptionPane.showMessageDialog(null, output, "TO DO", 3);
+	public static void main(String[] args) throws FileNotFoundException {
+		new HangMan().start();
+	}
 
-			//} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				//e1.printStackTrace();
-			//}
-			
+	ArrayList<String> wordsUsed = new ArrayList<String>();
+
+	public void start() {
+		ArrayList<String> allWords = readFile("dictionary.txt");
+		System.out.println("test");
+
+		int lengthOfList = getNumber();
+		for (int i = 0; i < lengthOfList; i++) {
+			Random rand = new Random();
+			int wordPicked = rand.nextInt(allWords.size());
+			wordsUsed.add(allWords.get(wordPicked));
+			System.out.println(wordsUsed);
 		}
-		
-		
+		build();
+	}
 
-	public int SetNumber() {
+	public void build() {
+		JFrame f = new JFrame();
+		JPanel p = new JPanel();
+		JTextArea t = new JTextArea();
+		JLabel l = new JLabel();
+		f.add(t);
+		f.add(l);
+		f.setSize(500,200);
+		f.show();
+
+		for (int i = 0; i < wordsUsed.size(); i++) {
+			int lives = 9;
+
+			while (lives != 0) {
+
+				l.setText("YOU HAVE " + lives + " LIVES REMAINING");
+			}
+		}
+	}
+
+	public int getNumber() {
 		int length = 0;
 		while (length == 0) {
-			String listLength = JOptionPane.showInputDialog("Yo my guy, how many words would you like to be on the list?");
+			String listLength = JOptionPane
+					.showInputDialog("Yo my guy, how many words would you like to be on the list?");
 			try {
 				length = Integer.parseInt(listLength);
 			} catch (NumberFormatException e) {
@@ -47,6 +67,30 @@ public class HangMan {
 		}
 		System.out.println(length);
 		return length;
+	}
+
+	public ArrayList<String> readFile(String file) {
+		ArrayList<String> words = new ArrayList<String>();
+		FileReader fr = null;
+		try {
+			fr = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fr);
+			String output = "";
+
+			while ((output = reader.readLine()) != null) {
+				words.add(output);
+			}
+
+			fr.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return words;
 	}
 
 }
