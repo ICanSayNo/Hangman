6d96a6c978a647506259af6c12fe72cb00d36388
ICanSayNo <league@WTS7.attlocal.net>
2018-06-25 20:02:38 -0700

    finished

--- src/HangMan.java
+++ src/HangMan.java
@@ -24,11 +24,11 @@ public class HangMan implements KeyListener {
 	ArrayList<String> allWords = readFile("dictionary.txt");
 	ArrayList<String> wordsUsed = new ArrayList<String>();
 	ArrayList<String> lettersUsed = new ArrayList<String>();
+	int lives = 10;
 	JFrame f = new JFrame();
 	JPanel p = new JPanel();
-	JLabel t = new JLabel();
-	JLabel l = new JLabel();
-	int lives = 30;
+	JLabel t = new JLabel("");
+	JLabel l = new JLabel("YOU HAVE " + lives + " LIVES REMAINING");
 	int currentWord;
 
 	public void start() {
@@ -46,12 +46,15 @@ public class HangMan implements KeyListener {
 	}
 
 	public void build() {
-
+		f.setLayout(null);
+		t.setBounds(30, 50, 400, 30);
 		f.add(t);
+		l.setBounds(30, 120, 400, 30);
 		f.add(l);
 		f.setSize(500, 200);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.addKeyListener(this);
+		t.setText(word(currentWord));
 		f.setVisible(true);
 	}
 
@@ -71,7 +74,7 @@ public class HangMan implements KeyListener {
 				word = word + "_ ";
 		}
 		System.out.println(word);
-
+		
 		return word;
 	}
 
@@ -121,7 +124,50 @@ public class HangMan implements KeyListener {
 			t.setText(word(currentWord));
 			if (!wordsUsed.get(currentWord).contains(x)) lives--;
 			l.setText("YOU HAVE " + lives + " LIVES REMAINING");
+			if(lives==0) {
+				JOptionPane.showMessageDialog(null, "My boi the word was "+wordsUsed.get(currentWord)+". You done goofed.");
+				t.setText("");
+				wordsUsed.clear();
+				lettersUsed.clear();
+				lives = 10;
+				start();
+			}
+			if (decrypt(t.getText()).equals(wordsUsed.get(currentWord))) {
+				JOptionPane.showMessageDialog(null, "Noice! You got the word! ON TO THE NEXT ONE!!!");
+				currentWord+=1;
+				lettersUsed.clear();
+				lives = 10;
+				System.out.println(wordsUsed.size()-1);
+				System.out.println(currentWord);
+			}
+			if (currentWord>(wordsUsed.size()-1)) {
+				JOptionPane.showMessageDialog(null, "YOU HAS WON! WOOO! Now do it again. Forever.");
+				t.setText("");
+				wordsUsed.clear();
+				lettersUsed.clear();
+				lives = 10;
+				start();
+			}
+			t.setText(word(currentWord));
 		}
+	}
+
+	public String decrypt(String text) {
+		String decryptedText = "";
+		boolean letterFound;
+		for (char c : text.toCharArray()) {
+			letterFound = false;
+				if (!(c + "").equals(" "))
+					letterFound = true;
+
+			if (letterFound)
+				decryptedText = (decryptedText + c );
+
+			else
+				decryptedText = decryptedText;
+		}
+		System.out.println(decryptedText);
+		return decryptedText;
 	}
 
 	@Override
